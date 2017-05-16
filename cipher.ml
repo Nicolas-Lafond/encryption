df742d6ae9e5754ce167bead4020eb24056a1dd8
Nicolas_Lafond <lafondni89@gmail.com>
2017-05-16 11:40:10 -0400

    added cesar cipher

---
+++ cipher.ml
@@ -0,0 +1,46 @@
+(* a datatype for letter 
+ * the int value is the order of the letter in the alphabet *)
+type letter = 
+    | Upper_Case of int
+    | Lower_Case of int
+    | Not_A_Letter 
+
+let char_to_letter c =
+    let char_value = int_of_char c in
+    if char_value <= int_of_char 'Z' && char_value >= int_of_char 'A' 
+        then Upper_Case (char_value - int_of_char 'A')
+    else if char_value <= int_of_char 'z' && char_value >= int_of_char 'a' 
+        then Lower_Case (char_value - int_of_char 'a')
+    else Not_A_Letter
+
+let letter_to_char l = match l with
+    | Upper_Case o -> char_of_int (o + int_of_char 'A')
+    | Lower_Case o -> char_of_int (o + int_of_char 'a')
+    | Not_A_Letter -> ""
+
+let string_to_list str = 
+    let aux s l i len = 
+        if i < len 
+        then aux s (l @ s.[i]) (i + 1) len
+        else l
+    in aux str [] 0 (String.length str)
+
+let _right_shift letter number = match letter with
+    | Upper_Case o 
+        -> if o > 25 then Upper_Case (o - 26)
+                     else Upper_Case o
+    | Lower_Case o
+        -> if o > 25 then Lower_Case (o - 26)
+                     else Lower_Case o
+    | Not_A_Letter -> Not_A_Letter
+
+let right_shift number character = 
+    let number = number mod 26 in
+    let letter = char_to_letter character in
+    match letter with 
+        | Not_A_Letter -> character
+        | _ -> letter_to_char (_right_shift letter number)
+    
+
+let cesar_cipher message shift_val = 
+    String.map (right_shift shift_val) message
